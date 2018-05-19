fad3be7e59a46e9da8b8d2e17902b615fa60ff9f
Sven-Koene <32331946+Sven-Koene@users.noreply.github.com>
2018-05-19 15:28:30 +0200

    basics voor mainpage

--- index.php
+++ index.php
@@ -8,6 +8,48 @@
     <title>Document</title>
 </head>
 <body>
+
+<!-- misschien met javascript een fotoslider met een foto van iedereen die mee doet? -->
+    <img src="images/download.jpg" class = slider>
     
+<!-- navbar kan beter gestyled worden als er ideeën zijn -->
+    <div class="navbar">
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="Sven.php">Sven</a></li>
+        <li><a href="Romano.php">Romano</a></li>
+        <li><a href="Defne.php">Defne</a></li>
+        <li><a href="iemand.php">Iemand</a></li>
+        <li style="float: right"><a href="register.php">registreer</a></li>
+        <li style="float: right"><a href="login.php">Login</a></li>
+    </ul>
+
+<!-- ff met materialise ofzoiets containers van maken en stylen -->
+</div>
+    <h1>Titel</h1>
+<div>
+    <h2>participant1</h2>
+    <p>omschrijving van jezelf</p> 
+</div>
+<div>
+    <h2>participant2</h2> 
+    <p>omschrijving van jezelf</p>
+</div>
+<div> 
+    <h2>participant3</h2>
+    <p>omschrijving van jezelf</p>
+</div>
+<div>    
+    <h2>participant4</h2>
+    <p>omschrijving van jezelf</p>
+</div>
+<br>
+<br>
+<br>
+
+<!-- footer met eventueel links naar social media en linked in accounts? -->
+<footer>
+    <p>links naar dingen per persoon</p>
+</footer>
 </body>
 </html>
