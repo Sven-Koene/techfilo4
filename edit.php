0ed30639477b172a4db0668bf61962d93f238a57
Sven-Koene <32331946+Sven-Koene@users.noreply.github.com>
2018-06-05 16:25:04 +0200

    edit error opgelost

--- edit.php
+++ edit.php
@@ -7,7 +7,7 @@ require_once "includes/database.php";
 //Check if Post isset, else do nothing
 if (isset($_POST['submit'])) {
     //Postback with the data showed to the user, first retrieve data from 'Super global'
-    $id = mysqli_real_escape_string($db, $_POST['id']);
+    $userId = mysqli_real_escape_string($db, $_POST['id']);
     $first = mysqli_real_escape_string($db, $_POST['first']);
     $last = mysqli_real_escape_string($db, $_POST['last']);
     $date = mysqli_real_escape_string($db, $_POST['date']);
@@ -27,11 +27,11 @@ if (isset($_POST['submit'])) {
     if (empty($errors)) {
         //Update the record in the database
         $query = "UPDATE users
-                  SET first = '$first',
-                  last = '$last',
-                  date = '$date',
-                  email = '$email',
-                  WHERE id = '$id'";
+                  SET `first` = '$first',
+                  `last` = '$last',
+                  `date` = '$date',
+                  `email` = '$email'
+                  WHERE `id` = '$userId'";
         $result = mysqli_query($db, $query);
 
         if ($result) {
@@ -43,10 +43,10 @@ if (isset($_POST['submit'])) {
     }
 } else {
     //Retrieve the GET parameter from the 'Super global'
-    $id = $_GET['id'];
+    $userId = $_GET['id'];
 
     //Get the record from the database result
-    $query = "SELECT * FROM users WHERE id = " . mysqli_escape_string($db, $id);
+    $query = "SELECT * FROM users WHERE id = " . mysqli_escape_string($db, $userId);
     $result = mysqli_query($db, $query);
     $user = mysqli_fetch_assoc($result);
 }
@@ -88,7 +88,7 @@ mysqli_close($db);
     </div>
 
     <div class="data-submit">
-        <input type="hidden" name="id" value="<?= $id; ?>"/>
+        <input type="hidden" name="id" value="<?= $userId; ?>"/>
         <input type="submit" name="submit" value="Save"/>
     </div>
 </form>
